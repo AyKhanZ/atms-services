5e73707ade8ca5376110ad285211741bb80c7dbc
zeynalovayxan70@gmail.com <zeynalovayxan70@gmail.com>
2026-02-19 17:51:56 +0400

    Users Controller added

---
+++ src/ATMS.Admin/ATMS.Admin.Service/Handlers/Users/GetUserHandler.cs
@@ -0,0 +1,26 @@
+﻿using ATMS.Admin.Contracts.Models;
+using ATMS.Admin.Contracts.Requests.Users;
+using ATMS.Admin.Data.Repositories;
+using ATMS.Exceptions.Entity;
+using AutoMapper;
+using MediatR;
+
+namespace ATMS.Admin.Service.Handlers.Users;
+
+public class GetUserHandler(
+    UserRepository userRepository,
+    Mapper mapper
+    ) : IRequestHandler<GetUserRequest, UserModel>
+{
+    public async Task<UserModel> Handle(GetUserRequest request, CancellationToken cancellationToken)
+    {
+        var result = await userRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (result is null)
+        {
+            throw new EntityException(EntityErrorType.EntityNotFound, $"User with id {request.Id} not found");
+        }
+
+        return mapper.Map<UserModel>(result);
+    }
+}
---
+++ src/ATMS.Admin/ATMS.Admin.Service/Handlers/Users/GetUsersHandler.cs
@@ -0,0 +1,20 @@
+﻿using ATMS.Admin.Contracts.Models;
+using ATMS.Admin.Contracts.Requests.Users;
+using ATMS.Admin.Data.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace ATMS.Admin.Service.Handlers.Users;
+
+public class GetUsersHandler(
+    UserRepository userRepository,
+    Mapper mapper
+    ) : IRequestHandler<GetUsersRequest, UserModel[]>
+{
+    public async Task<UserModel[]> Handle(GetUsersRequest request, CancellationToken cancellationToken)
+    {
+        var result = await userRepository.GetAsync();
+
+        return mapper.Map<UserModel[]>(result);
+    }
+}
